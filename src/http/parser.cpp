deb09b798679bdee483e78045d379498ce59a242
kusa <th.kusa87@gmail.com>
2023-10-28 21:56:07 +0900

    updated

--- src/http/parser.cpp
+++ src/http/parser.cpp
@@ -12,17 +12,42 @@ Request Parser::parse(string data)
 {
     Request request;
     string request_data = data;
-    string delimiter = "\r\n";
-    int del_length = delimiter.length();
-    vector<string> lines = {};
+    string crlf = "\r\n";
+    string space = " ";
+    string header_sep = ": ";
+    int crlf_len = crlf.length();
+    int space_len = space.length();
+    int sep_len = header_sep.length();
+    vector<string> headers = {};
 
-    while (int pos = request_data.find(delimiter) != string::npos) {
+    int pos = request_data.find(crlf);
+
+    while (pos != string::npos) {
+        if (pos == 0) {
+            request.body = request_data.substr(crlf_len);
+            break;
+        }
         string line = request_data.substr(0, pos);
-        lines.emplace_back(line);
-
-        request_data = request_data.substr(pos + del_length);
+        headers.emplace_back(line);
+        request_data = request_data.substr(pos + crlf_len);
+        pos = request_data.find(crlf);
     }
-    
+
+    string start_line = headers[0];
+    int method_pos = start_line.find(space);
+    request.method = start_line.substr(0, method_pos);
+    start_line = start_line.substr(method_pos + space_len);
+    int target_pos = start_line.find(space);
+    request.target = start_line.substr(0, target_pos);
+    request.version = start_line.substr(target_pos + space_len);
+
+    for (int i = 1; i < headers.size(); i++) {
+        string header = headers[i];
+        int sep_pos = header.find(header_sep);
+        string left = header.substr(0, sep_pos);
+        string right = header.substr(sep_pos + sep_len);
+        request.fields[left] = right;
+    }
     return request;
 }
 
@@ -32,7 +57,7 @@ string Parser::build(Response response)
     string first = response.version + " " + to_string(response.status) + "\r\n";
     result += first;
     for (pair<string, string> field : response.fields) {
-        string header = field.first + ":" + field.second + "\r\n";
+        string header = field.first + ": " + field.second + "\r\n";
         result +=  header;
     }
     result += "\r\n";
--- src/server.cpp
+++ src/server.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <iostream>
+#include <filesystem>
 #include "connection/socket.h"
 #include "http/parser.h"
 #include "http/request.h"
 #include "http/response.h"
 
 using namespace std;
+using namespace std::filesystem;
 using namespace Connection;
 using namespace Http;
 
 int main() {
-    
+    string document_root = "./html";
+    string default_document = "index.html";
     Parser parser;
     cout << "Start simple HTTP Server" << endl;
     while (true) {
@@ -22,11 +27,31 @@ int main() {
         cout << "--------------------" << endl;
 
         Request request = parser.parse(data);
+        string document = request.target;
+        if (document == "/") {
+            document += default_document;
+        }
+
+        
         Response response;
         response.version = "HTTP/1.1";
         response.status = 200;
-        response.fields["Content-Length"] = "13";
-        response.body = "Hello World";
+        response.fields["Host"] = request.fields["Host"];
+
+        string path = document_root + document;
+        if (exists(path)) {
+            ifstream reading_file;
+            reading_file.open(path, ios::in);
+            string reading_line_buffer;
+            while(getline(reading_file, reading_line_buffer)){
+                response.body += reading_line_buffer + "\r\n";
+            }
+        } else {
+            response.status = 404;
+            response.body = "404 Not Found";
+        }
+
+        response.fields["Content-Length"] = to_string(response.body.length());
         string result = parser.build(response);
         cout << "----- Response -----" << endl;
         cout << result << endl;
